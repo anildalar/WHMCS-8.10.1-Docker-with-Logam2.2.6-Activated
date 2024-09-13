0f7847001097c98f88bf70e725e13aa5a62e995f
ANIL DOLLOR <dollorinfotech@gmail.com>
2024-09-13 14:29:36 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,6 @@ export EDITOR=nano
 # Set permissions for WHMCS directories
 chown -R www-data:www-data /var/www/html
 chmod -R 755 /var/www/html
-chmod -R 775 /var/www/html/configuration.php /var/www/html/attachments /var/www/html/downloads /var/www/html/templates_c
 
 # Start cron service
 service cron start
